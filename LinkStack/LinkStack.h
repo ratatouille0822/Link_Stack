965ee3c06b87a58a9132b6e57d2343e713ed9af1
Ratatouille <307338947@qq.com>
2019-04-07 21:22:43 +0800

    管理块的声明类型改为void*

--- LinkStack/LinkStack.h
+++ LinkStack/LinkStack.h
@@ -16,10 +16,10 @@ typedef void* LinkStackType;
 typedef void* DataType;
 
 LinkStackType Init_LinkStack();
-int Push_LinkStack(pLinkStackMan link_stack_manager, DataType data);
-int Pop_LinkStack(pLinkStackMan link_stack_manager);
-DataType GetTopElement_LinkStack(pLinkStackMan link_stack_manager);
-int GetSize_LinkStack(pLinkStackMan link_stack_manager);
-int IsEmpty_LinkStack(pLinkStackMan link_stack_manager);
-int Free_LinkStack(pLinkStackMan link_stack_manager);
+int Push_LinkStack(LinkStackType link_stack_manager, DataType data);
+int Pop_LinkStack(LinkStackType link_stack_manager);
+DataType GetTopElement_LinkStack(LinkStackType link_stack_manager);
+int GetSize_LinkStack(LinkStackType link_stack_manager);
+int IsEmpty_LinkStack(LinkStackType link_stack_manager);
+int Free_LinkStack(LinkStackType link_stack_manager);
 
